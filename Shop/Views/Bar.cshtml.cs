f3f81690472446a5fb58125166048d82828dd862
Opilane TTHK <101242957+Fil31@users.noreply.github.com>
2023-11-21 12:43:09 +0200

    bar start
    bar start

---
+++ Shop/Views/Bar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shop.Views
+{
+    public class BarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
